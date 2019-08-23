b48a96aaa733c23c4932e0dfde9dd5565c6fdb2a
Allen Li <lhf552004@163.com>
2019-08-23 09:57:17 +0800

    web2 swagger test api

---
+++ Web.API/Web2/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web2.Controllers
+{
+    public class TestController : ApiController
+    {
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        public void Get(int id)
+        {
+        }
+    }
+}
